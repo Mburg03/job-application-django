8b20ab77c9ae76ceddc3aef6a12f8cb19829054c
Mburg03 <mburgosgit003@gmail.com>
2023-12-30 12:50:15 -0600

    went crazy and added more fields and upgrades

--- job_application/models.py
+++ job_application/models.py
@@ -9,6 +9,13 @@ class Form(models.Model):
     email = models.EmailField()
     date = models.DateField()
     occupation = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    street_address = models.CharField(max_length=255, null=True, blank=True)
+    job_title = models.CharField(max_length=100, null=True, blank=True)
+    employer = models.CharField(max_length=100, null=True, blank=True)
+    employment_duration = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- job_application/views.py
+++ job_application/views.py
@@ -2,43 +2,66 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
-from dotenv import load_dotenv
 from django.core.mail import send_mail, EmailMessage
-from django.conf import settings
-import os
-
+from datetime import date
 
 # Create your views here.
+current_date = date.today()
+formatted_date = current_date.strftime('%Y-%d-%m')
 
 
 def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
         if form.is_valid():
+            print(f" second form pass {form.cleaned_data}")
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+            # Phone
+            phone = form.cleaned_data["phone_number"]
+            # City
+            city = form.cleaned_data["city"]
+            # Address
+            address = form.cleaned_data["street_address"]
+            # Age
+            age = form.cleaned_data["age"]
+            # last job title
+            last_job_title = form.cleaned_data["job_title"]
+            # last employer
+            last_employer = form.cleaned_data["employer"]
+            # duration of employment
+            duration_of_last_employment = form.cleaned_data["employment_duration"]
+
 
             message_body = f"""
             Hey, {first_name}! Thanks for using our platform! 😄
             Here you have your information: 
+            
             Your complete name: {first_name} {last_name}. 
             Your current occupation: {occupation}
+            
             I hope this to be helpful, have a great day! 🫶🏻
             """
 
             email_to_send = EmailMessage(subject="New form submission! 📩", body=message_body, to=[email])
             email_to_send.send()
-
-            Form.objects.create(first_name=first_name, last_name=last_name, email=email, occupation=occupation,
-                                date=date)
+            try:
+                Form.objects.create(first_name=first_name, last_name=last_name, email=email, occupation=occupation,
+                                    date=date, phone_number=phone, age=age, city=city, street_address=address,
+                                    job_title=last_job_title, employer=last_employer,
+                                    employment_duration=duration_of_last_employment)
+            except Exception as e:
+                print(f"Error saving form data: {e}")
 
             messages.success(request,
                              "Form submitted successfully!")
-
-    return render(request, template_name='index.html')
+        else:
+            print(form.errors)
+    print("just here")
+    return render(request, template_name='index.html', context={'current_date': formatted_date})
 
 
 def about(request):
