49a5a28848f157f018f36023797f75c320b112cc
Mburg03 <mburgosgit003@gmail.com>
2023-12-29 20:12:48 -0600

    grabbing user information from front-end

--- job_application/views.py
+++ job_application/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name, last_name, email, date,occupation)
+
     return render(request, template_name='index.html')
+
