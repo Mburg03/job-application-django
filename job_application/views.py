1fdd4f75c3659feba3019e0ceb57a9e5e1aba820
Mburg03 <mburgosgit003@gmail.com>
2023-12-29 20:24:28 -0600

    User information now stored in the dabase

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
-
+from .models import Form
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,7 +14,10 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name, last_name, email, date,occupation)
+
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, occupation=occupation,
+                                date=date)
+            messages.success(request,
+                             "Form submitted successfully!")
 
     return render(request, template_name='index.html')
-
